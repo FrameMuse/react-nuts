9f4fabefba22f9980ae7e6bb460b787f86d5ec04
FrameMuse <minicablestone@gmail.com>
2021-07-17 05:33:51 +0300

    view sample change

--- bin/samples/view.sample.tsx
+++ bin/samples/view.sample.tsx
@@ -1,11 +1,9 @@
 import type { ViewProps } from "react-nuts/src/components/ViewRouter/interfaces"
 interface $ViewNameParams { }
-function $ViewName(props: ViewProps<$ViewNameParams>) {
+export default function $ViewName(props: ViewProps<$ViewNameParams>) {
   return (
     <>
 
     </>
   )
 }
-
-export default $ViewName
